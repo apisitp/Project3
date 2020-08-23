4192eab7d7f6d4c5bdb840fad890cb40be1922f7
apisit pukinghin <apisit pukinghin@LAPTOP-EEV4BUFC>
2020-08-23 23:05:09 +0700

    Add project files.

---
+++ Project3/Source.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int x, y, n;
+	printf("enter fist number ");
+	scanf("%d", &x);
+	printf("enter second number");
+	scanf("%d", &y);
+	n = x + y;
+	printf("n of %d +%d is %d\n", x, y, n);
+	return 0;
+		
+
+}
